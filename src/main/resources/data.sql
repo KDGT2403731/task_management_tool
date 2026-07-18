205d3789948e4543f781c9316ed12f735c67b8ea
KDGT2403731 <shibakawayuya@gmail.com>
2026-07-18 15:04:23 +0900

    data.sql変更

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (id, name, email, password_hash, role)
 VALUES
-(1, '山田花子', 'hanakoyama@gmail.com', '$2a$10$boCZZD6yFIm1rQW8y7xP0.hknPiNx1Df03FR/JCDZYrCGXIEFkS2S', 'ADMIN'),
-(2, '鈴木太郎', 'tarosuzuki@gmail.com', '$2a$10$5QjLHffMYShnPWHtQHbmgeXixaem/e7GQmjHdIT.PEXey7CW9q.la', 'MEMBER');
+(1, '山田花子', 'hanakoyama@gmail.com', '$2a$10$1ijVGK5Gufw6J3FRgaoG.een8WaJi2FwgluCZxyfCaWUhL54w.wOi', 'ADMIN'),
+(2, '鈴木太郎', 'tarosuzuki@gmail.com', '$2a$10$s530/bQG1WUmOQMmmUqECeaowzNUkqVF1g3V3LDZU34SSkW5mPnTq', 'MEMBER');
  
 -- BIGSERIAL(id)へ明示的に値を挿入すると、内部シーケンスがそれを認識しないため、
 -- 後続のアプリからのINSERT（サインアップ等）でid重複エラーになる可能性がある。
